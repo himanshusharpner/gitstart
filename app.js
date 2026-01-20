512e6b7f87bf44b18f224dcd0b601ade9ef4e043
himanshusharpner <himanshuravigwr@gmail.com>
2026-01-20 22:58:46 +0530

    first commit

--- app.js
+++ app.js
@@ -5,3 +5,10 @@ header.style.borderBottom ="3px solid orange";
 
 const mainHeading = document.getElementById("main-heading");
 mainHeading.textContent = "Fresh fruits Shop";
+
+const basketheading = document.getElementById("basket-heading");
+basketheading.style.fontStyle = "Italic";
+
+const thanks = document.getElementById("thanks");
+thanks.innerHTML = '<p> Thank you for visiting us</p>';
+thanks.style.fontWeight = "bold";
