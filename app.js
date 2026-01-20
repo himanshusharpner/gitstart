4d9e2e6e7bb9458fdfb78b66816ea5a54d5ad8aa
himanshusharpner <himanshuravigwr@gmail.com>
2026-01-20 22:36:12 +0530

    first commit

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-console.log("My first git work ");
+const header = document.getElementById("header");
+header.style.backgroundColor = "red";
+header.style.color = "white";
+header.style.borderBottom ="3px solid orange";
+
+const mainHeading = document.getElementById("main-heading");
+mainHeading.textContent = "Fresh fruits Shop";
